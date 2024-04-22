a82cb639c66fa29c8c7e2afd55347eda8e308ba2
mbrraghu <mbrraghu@gmail.com>
2024-04-22 09:04:32 +0530

    first commit

---
+++ crm_displayname_swapping/__init__.py
@@ -0,0 +1,5 @@
+# coding: UTF-8
+
+from . import res_partner
+
+
---
+++ crm_displayname_swapping/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+{
+    'name' : 'Display Name Swapping',
+    'version' : '1.0',
+    'description':"""
+    v1.0 - overriding the method of display name in the res.partner model
+    """,
+    'summary':'helps to view the name first and company name',
+    'category': 'CRM',
+    'depends' : ['stock'],
+    'data': [],
+    'installable': True,
+    'application': False,
+    'license': 'LGPL-3',
+}
